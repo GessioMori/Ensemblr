9fec4a6458265ef7896e48cdc3b5c4ce5eea9b53
Gessio Mori <gessiomorin@gmail.com>
2025-09-15 15:42:56 -0300

    Add SQL Server resource to AppHost and solution
    Integrated SQL Server into the AppHost project with persistent container lifetime and data volume. Updated the API and web project dependencies to reference and wait for the database. Added Aspire.Hosting.SqlServer package and included new projects in the solution file.

--- src/Ensemblr.AppHost/AppHost.cs
+++ src/Ensemblr.AppHost/AppHost.cs
@@ -1,14 +1,24 @@
 
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-        IResourceBuilder<ProjectResource> apiProject = builder.AddProject<Projects.Ensemblr_Api>("ensemblr-api");
+        IResourceBuilder<SqlServerServerResource> sql = builder
+            .AddSqlServer("sql-server").WithLifetime(ContainerLifetime.Persistent)
+            .WithDataVolume();
+
+        IResourceBuilder<SqlServerDatabaseResource> db = sql.AddDatabase("ensemblr-db");
+
+        IResourceBuilder<ProjectResource> apiProject = builder.AddProject<Projects.Ensemblr_Api>("ensemblr-api")
+            .WithReference(db)
+            .WaitFor(db);
 
         IResourceBuilder<NodeAppResource> webProject = builder.AddNpmApp("ensemblr-react", "../Ensemblr.Web")
-            .WithHttpsEndpoint(env: "VITE_PORT")
+            .WithHttpsEndpoint(port: 5173, env: "VITE_PORT")
+            .WaitFor(apiProject)
             .WithReference(apiProject);
 
         builder.Build().Run();
